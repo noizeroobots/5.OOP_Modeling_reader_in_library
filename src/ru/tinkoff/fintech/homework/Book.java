f0923f6c1bd2679e41f0c2658a7e116bd004b56a
noizeroobots <noizeroobots@gmail.com>
2021-02-26 00:46:37 +0300

    changed: the beautiful view in console

--- src/ru/tinkoff/fintech/homework/Book.java
+++ src/ru/tinkoff/fintech/homework/Book.java
@@ -4,17 +4,37 @@ public class Book extends Main{
 
     String name;
     String author;
+    int year;
     int order;
     int pages;
 
-    public Book(String n) {
-        name = n;
-    }
-    public Book(String n, String a) {
+    public Book(String n, String a) {  //Перегруженный конструктор №1
         name = n;
         author = a;
     }
+    public Book(String n, String a, int y) {  //Перегруженный конструктор №2
+        name = n;
+        author = a;
+        year = y;
+    }
+    public Book(String n, String a, int y, int p) {  //Перегруженный конструктор №3
+        name = n;
+        author = a;
+        year = y;
+        pages = p;
+    }
+    public Book(String n, String a, int y, int p, int o) {  //Перегруженный конструктор №4
+        name = n;
+        author = a;
+        year = y;
+        pages = p;
+        order = o;
+    }
 
+    @Override
+    public String toString() {
+        return author + " - " + name + ". - " + year + " г. - " + pages + " с. " + order + ". ";
+    }
 }
 // Java-конструктор - это специальный метод, который вызывается при создании экземпляра класса.
 //Book common = new Book("Idiot", "Dostoevsky", 1, 99);
--- src/ru/tinkoff/fintech/homework/Main.java
+++ src/ru/tinkoff/fintech/homework/Main.java
@@ -4,11 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Book book1 = new Book("Idiot");
-        System.out.println(book1.name);
-        Book book2 = new Book("1984", "George Orwell");
-        System.out.println(book2.name + " " + book2.author);
-        Book book3 = new Book("Romeo and Juliet");
-        System.out.println(book3.name);
+        Book book1 = new Book("Idiot", "Fedor Dostoevsky");
+        Book book2 = new Book("1984", "George Orwell", 1949);
+        Book book3 = new Book("Romeo and Juliet", "William Shakespeare", 1595, 318);
+        Book book4 = new Book("Master and Margarita", "Michail Bulgakov", 1920, 328, 1);
+        System.out.println(book1.toString()); // Переопределение метода toString для класса Book.
+        System.out.println(book2.toString()); // Переопределение метода toString для класса Book.
+        System.out.println(book3.toString()); // Переопределение метода toString для класса Book.
+        System.out.println(book4.toString()); // Переопределение метода toString для класса Book.
     }
+
+
 }
