4a692f90fd2d43e29bc1bc6cfa38215502ddeb1a
noizeroobots <noizeroobots@gmail.com>
2021-02-28 15:06:53 +0300

    Book5, before Andrey's advice

--- src/ru/tinkoff/fintech/homework/Main.java
+++ src/ru/tinkoff/fintech/homework/Main.java
@@ -13,6 +13,9 @@ public class Main {
         System.out.println("Enter 'f' if you want to choose a random book: ");
         String action1 = in.nextLine();
         String f = "f";
+        String[] list = new String[4];
+        Reader.choiceBook(list);
+
 
         while (action1.indexOf("f") == -1) {
             for (int i = 0; i < f.length(); i++) {
@@ -21,23 +24,11 @@ public class Main {
                 Book book2 = new Book("1984", "George Orwell", 1949);
                 Book book3 = new Book("Romeo and Juliet", "William Shakespeare", 1595, 318);
                 Book book4 = new Book("Master and Margarita", "Michail Bulgakov", 1920, 328, 1);
-//        System.out.println(book1.toString()); // Переопределение метода toString для класса Book.
-//        System.out.println(book2.toString()); // Переопределение метода toString для класса Book.
-//        System.out.println(book3.toString()); // Переопределение метода toString для класса Book.
-//        System.out.println(book4.toString()); // Переопределение метода toString для класса Book.
 
-                String[] list = new String[4]; // Из списка книг по их названию их же вытащил и создал массив
                 list[0] = book1.name;
                 list[1] = book2.name;
                 list[2] = book3.name;
                 list[3] = book4.name;
-//                for (int i = 0; i < 4; i++) {
-//                    System.out.println("Book: " + list[i]);
-//                }
-                Random random = new Random(); // Мой рандом работает!
-                int index = random.nextInt(list.length);
-                System.out.println(list[index]);
-
             }
             return;
         }
--- src/ru/tinkoff/fintech/homework/Reader.java
+++ src/ru/tinkoff/fintech/homework/Reader.java
@@ -2,9 +2,12 @@ package ru.tinkoff.fintech.homework;
 
 import java.util.Random;
 
-public class Reader {
-
-//    public choiceBook() {
-//
-//    }
+public class Reader extends Main{
+    public static String[] choiceBook(String[] list) {
+        Random random = new Random();
+        int index = random.nextInt(list.length);
+        String name = list[index];
+        System.out.println("topol " + name);
+        return list;
+    }
 }
